f95f6a6fc58cd1660047af08905a285036f8e8ef
proxiner <im.proxiner@gmail.com>
2022-05-12 00:19:15 +0430

    change added

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ const options = {
     timeout: 15000,
 }
 
+const locations = {
+    homeLat: 48.856614,
+    homeLong: 2.3522219,
+    officeLat: null,
+    officeLong: null,
+}
+
 const result = document.querySelector("#result");
 
+const location = document.querySelector("#location");
 
 const success = (position) => {
     const coords = position.coords;
     result.textContent = `latitude : ${coords.latitude} & longitude : ${coords.longitude}`;
+
+    if (coords.latitude === locations.homeLat && coords.longitude === locations.homeLong) {
+        location.textContent = "Your at home";
+    }
 }
 
 const error = (errorLog) => {
