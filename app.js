ffbb02e80a9143e2973d7bc736cf6528ef733ad4
proxiner <im.proxiner@gmail.com>
2022-05-12 01:49:36 +0430

    fdfdf

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 let locations = {
     // homeLat: 35.6719733,
     // homeLong: 51.0401086,
-    homeLat: 35,
+    homeLat: 36,
     homeLong: 51,
     officeLat: null,
     officeLong: null,
@@ -15,6 +15,9 @@ const options = {
 }
 
 const success = (position) => {
+    let today = new Date();
+    let timeNow = today.getHours() + ":" + ("0" + today.getMinutes()).slice(-2);
+
     let myLocation = document.querySelector("#location");
     const coords = position.coords;
     alert(`lat : ${Math.round(coords.latitude)} & Long : ${Math.round(coords.longitude)}`)
@@ -22,7 +25,8 @@ const success = (position) => {
 
     if (coords.latitude === locations.homeLat && coords.longitude === locations.homeLong) {
         myLocation.textContent = "Your at Home";
-        window.open("https://www.itmabna.com");
+        let result = `🏢 ورود به شرکت آی تی مبنا                                            🕰️ ساعت ${timeNow}`;
+        let win = window.open(`https://api.whatsapp.com/send?phone=989123657098&text=${result}&source=&data=`);
     } else {
         myLocation.textContent = "Your Not at Home"
     }
