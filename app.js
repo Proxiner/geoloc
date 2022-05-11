aeab53f2a76b6b14e324ecd1e1492f0576038993
proxiner <im.proxiner@gmail.com>
2022-05-12 01:02:39 +0430

    22-a

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+navigator.geolocation.getCurrentPosition(success, error, options);
+
 const options = {
     maximumAge: 0,
     enableHighAccuracy: false,
     timeout: 15000,
+}
+
+let result = document.querySelector("#result");
+
+
+const success = (position) => {
+    let coords = position.coords;
+    result.textContent = `Latitude : ${coords.latitude} & Longitude : ${coords.longitude}`;
+}
+
+
+const error = (errorLog) => {
+    console.log(errorLog);
 }
 
 // const locations = {
@@ -11,14 +26,8 @@ const options = {
 //     officeLong: null,
 // }
 
-let result = document.querySelector("#result");
 
 // let myLocation = document.querySelector("#location");
-
-const success = (position) => {
-    const coords = position.coords;
-
-    result.textContent = `Latitude : ${coords.latitude} & Longitude : ${coords.longitude}`;
 
     // if (coords.latitude === locations.homeLat && coords.longitude === locations.homeLong) {
     //     myLocation.textContent = "Your at Home";
@@ -26,10 +35,4 @@ const success = (position) => {
     // } else {
     //     myLocation.textContent = "Your Not at Home"
     // }
-}
 
-const error = (errorLog) => {
-    console.log(errorLog);
-}
-
-navigator.geolocation.getCurrentPosition(success, error, options);
