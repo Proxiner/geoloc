462136c3e0924fc7f3ef1f0dca8b7fea2c1e02de
proxiner <im.proxiner@gmail.com>
2022-05-12 01:00:02 +0430

    23

--- app.js
+++ app.js
@@ -4,28 +4,28 @@ const options = {
     timeout: 15000,
 }
 
-const locations = {
-    homeLat: 48.856614,
-    homeLong: 2.3522219,
-    officeLat: null,
-    officeLong: null,
-}
+// const locations = {
+//     homeLat: 48.856614,
+//     homeLong: 2.3522219,
+//     officeLat: null,
+//     officeLong: null,
+// }
 
 let result = document.querySelector("#result");
 
-let myLocation = document.querySelector("#location");
+// let myLocation = document.querySelector("#location");
 
 const success = (position) => {
     const coords = position.coords;
 
     result.textContent = `Latitude : ${coords.latitude} & Longitude : ${coords.longitude}`;
 
-    if (coords.latitude === locations.homeLat && coords.longitude === locations.homeLong) {
-        myLocation.textContent = "Your at Home";
-        // window.open("https://www.itmabna.com");
-    } else {
-        myLocation.textContent = "Your Not at Home"
-    }
+    // if (coords.latitude === locations.homeLat && coords.longitude === locations.homeLong) {
+    //     myLocation.textContent = "Your at Home";
+    //     // window.open("https://www.itmabna.com");
+    // } else {
+    //     myLocation.textContent = "Your Not at Home"
+    // }
 }
 
 const error = (errorLog) => {
