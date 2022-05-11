69a647772256c9325723a574831057239e74e840
proxiner <im.proxiner@gmail.com>
2022-05-12 01:17:16 +0430

    3e3e3e

--- app.js
+++ app.js
@@ -1,30 +1,14 @@
-
 const options = {
     maximumAge: 0,
     enableHighAccuracy: false,
     timeout: 15000,
 }
 
-
-let locations = {
-    homeLat: 48.856614,
-    homeLong: 2.3522219,
-    officeLat: null,
-    officeLong: null,
-}
-
 const success = (position) => {
     let result = document.querySelector("#result");
-    let myLocation = document.querySelector("#location");
     const coords = position.coords;
     result.textContent = `Latitude : ${coords.latitude} & Longitude : ${coords.longitude}`;
-    if (coords.latitude === locations.homeLat && coords.longitude === locations.homeLong) {
-        myLocation.textContent = "Your at Home";
-    } else {
-        myLocation.textContent = "Your Not at Home"
-    }
 }
-
 
 const error = (errorLog) => {
     console.log(errorLog);
@@ -32,4 +16,19 @@ const error = (errorLog) => {
 
 navigator.geolocation.getCurrentPosition(success, error, options);
 
+
+// let locations = {
+//     homeLat: 48.856614,
+//     homeLong: 2.3522219,
+//     officeLat: null,
+//     officeLong: null,
+// }
+
+// let myLocation = document.querySelector("#location");
+
+// if (coords.latitude === locations.homeLat && coords.longitude === locations.homeLong) {
+    //     myLocation.textContent = "Your at Home";
+    // } else {
+        //     myLocation.textContent = "Your Not at Home"
+        // }
 
